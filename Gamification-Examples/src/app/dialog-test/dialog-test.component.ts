4388b017f80ecdeb6dd9ea9cd5a8454de9f39e34
Arthur Leandro <ArthurLeandro@gmail.com>
2019-08-28 15:57:14 -0300

    Dialog

--- Gamification-Examples/src/app/dialog-test/dialog-test.component.ts
+++ Gamification-Examples/src/app/dialog-test/dialog-test.component.ts
@@ -1,3 +1,4 @@
+import{MatDialogRef} from '@angular/material';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogTestComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogBox :MatDialogRef<DialogTestComponent>) { }
 
   ngOnInit() {
   }
-
+  
+  OnClose(){
+    this.dialogBox.close();
+  }
+  
 }
--- Gamification-Examples/src/app/home/home.component.ts
+++ Gamification-Examples/src/app/home/home.component.ts
@@ -7,13 +7,19 @@ import { DialogTestComponent } from '../dialog-test/dialog-test.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+ 
+  
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    
   }
 
-  OpenDialogById(){ 
+  OpenDialogById(){
+    // const dialogConfig = new MatDialogConfig();
+    // dialogConfig.disableClose = true;
+    // dialogConfig.autoFocus = true;
+    // dialogConfig.width = "70%";
     this.dialog.open(DialogTestComponent);
   }
 }
