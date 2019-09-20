c2d98dae9b5d2e6c13b5b055baedc5ba2bcb4a36
Arthur Leandro <ArthurLeandro@gmail.com>
2019-09-20 15:55:48 -0300

    Termino das funcionalidades da Interação. Agora falta polir para a Interação estar completa. Porém eu irei começar os eventos para que eles fiquem todos com a funcionalidades prontas, e então restará somente a parte de Polir.

--- Gamification-Examples/src/app/home/home.component.ts
+++ Gamification-Examples/src/app/home/home.component.ts
@@ -11,47 +11,10 @@ import { DialogTestComponent } from '../dialog-test/dialog-test.component';
 export class HomeComponent implements OnInit {
 
   
-  constructor(private dialog: MatDialog , public dialogText:DialogTestComponent) { }
+  constructor() { }
 
   ngOnInit(){
 
   }
-  ConfigureDialog() : MatDialogConfig{
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "70%";
-    return dialogConfig;
-  }
-
-  OpenOnBoardingCard(){
-    this.dialogText.title = "On Boarding";
-    this.dialogText.textToBeShowned = "Este card cuida da iniciação do usuário com o sistema.";
-    this.dialog.open(DialogTestComponent, this.ConfigureDialog());
-  }
-  OpenPontuacaoCard(){
-    this.dialogText.title = "Pontuação";
-    this.dialogText.textToBeShowned = "Este card cuida da pontuação responsável por metrificar a experiência do usuário.";
-    this.dialog.open(DialogTestComponent, this.ConfigureDialog());
-  }
-  OpenMedalhasCard(){
-    this.dialogText.title = "Medalhas";
-    this.dialogText.textToBeShowned = "Este card cuida da recompensa para distinção das conquistas dos usuários mais engajados.";
-    this.dialog.open(DialogTestComponent, this.ConfigureDialog());
-  }
-  OpenMissoesCard(){
-    this.dialogText.title = "Missões";
-    this.dialogText.textToBeShowned = "Este card cuida da parte que será responsável por manter o usuário desafiado.";
-    this.dialog.open(DialogTestComponent, this.ConfigureDialog());
-  }
-  OpenNivelCard(){
-    this.dialogText.title = "Nível";
-    this.dialogText.textToBeShowned = "Este card cuida da parte responsável por destacar o engajamento do usuário.";
-    this.dialog.open(DialogTestComponent, this.ConfigureDialog());
-  }
-  OpenReferenciasCard(){
-    this.dialogText.title = "Referências";
-    this.dialogText.textToBeShowned = "Este card contém as referências utilizadas para efetuar a gamificação do sistema.";
-    this.dialog.open(DialogTestComponent, this.ConfigureDialog());
-  }
+  
 }
